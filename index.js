697e10bc8db7041630037c7b53a3c2f2eb000e2d
Alan Cinsavich <alan.cimsavich@gmail.com>
2021-07-23 09:38:26 -0400

    commit tree 7ee6a47b6b7b7a18a20f29e55a072c236f931ad3

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers,string) {
+    let answer = drivers.filter(el => el.toLowerCase() === string.toLowerCase())
+    return answer
+}
+   
+function fuzzyMatch(drivers, string) {
+    return drivers
+      .filter((driverName) => driverName.substring(0, string.length)  === string);
+  }
+
+  
